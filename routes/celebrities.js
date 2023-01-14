9aafc74ecbdc02791192d5d56be06f56ce29969e
Pau Bertolin <paulaberos@gmail.com>
2023-01-14 19:37:42 +0100

    add routes and views form iterartion 3

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,5 +1,28 @@
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
 
-// all your routes here
+
+/* GET Show a form to create a celebrity */
+/* ROUTE /celebrities/create/*/
+router.get('/create', async (req, res, next) => {
+  try {
+    res.render('celebrities/new-celebrity')
+  } catch (error) {
+    next(error)
+  }
+})
+
+/* POST get data from form -->create the celebrity --> save it database */
+/* ROUTE /celebrities/create/*/
+router.post('/create', async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  try {
+    await Celebrity.create({ name, occupation, catchPhrase });
+    res.redirect('/celebrities')
+  } catch (error) {
+    res.render('celebrities/new-celebrity')
+    //next(error)
+  }
+})
 
 module.exports = router;
