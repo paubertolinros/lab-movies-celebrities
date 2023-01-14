76bf5f71099688e201719597e0ac66e89ecb4fe7
Pau Bertolin <paulaberos@gmail.com>
2023-01-14 19:08:18 +0100

    create celebrity model - Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,27 @@
-//  Add your code here
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const celebritySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is mandatory. Please add a name']
+  },
+  occupation: {
+    type: String,
+    enum: {
+      values: ['actor', 'actress', 'musician', 'comediant', 'singer', 'dancer', 'unknown']
+    }, 
+    required: [true, 'Occupation is mandatory. Please select occupation']
+  },
+  catchPhrase: {
+    type: String,
+    required: [true, 'Catch phrase is mandatory. Please add catch phrase']
+  }
+},
+  {
+    timestamps: true
+  }
+)
+
+const Celebrity = mongoose.model('Celebrity', celebritySchema);
+module.exports = Celebrity;
