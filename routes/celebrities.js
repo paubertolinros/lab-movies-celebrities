2d99dbc6ccf939f21b1f334385297f5697c0749a
Pau Bertolin <paulaberos@gmail.com>
2023-01-24 18:00:57 +0100

    celebrities bonus 1 and 2

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model')
 
 
 /* GET Show all celebrities */
@@ -12,7 +13,7 @@ router.get('/', async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-})
+});
 
 /* GET Show a form to create a celebrity */
 /* ROUTE /celebrities/create/*/
@@ -22,7 +23,7 @@ router.get('/create', async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-})
+});
 
 /* POST get data from form -->create the celebrity --> save it database */
 /* ROUTE /celebrities/create/*/
@@ -35,8 +36,56 @@ router.post('/create', async (req, res, next) => {
     res.render('celebrities/new-celebrity')
     //next(error)
   }
-})
+});
 
 
+/* GET Show celebrity detail */
+/* ROUTE /celebrities/:celebrityId*/
+router.get('/:celebrityId', async (req, res, next) => {
+  const { celebrityId } = req.params;
+  try {
+    const celebrityDetail = await Celebrity.findById(celebrityId);
+    res.render('celebrities/celebrityDetail', celebrityDetail)
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET Update celebrity form */
+/* ROUTE /celebrities/edit/:celebrityId*/
+router.get('/edit/:celebrityId', async (req, res, next) => {
+  const { celebrityId } = req.params;
+  try {
+    const celebrityDetail = await Celebrity.findById(celebrityId);
+    res.render('celebrities/editCelebrity', celebrityDetail)
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* POST Update celebrity form */
+/* ROUTE /celebrities/edit/:celebrityId*/
+router.post('/edit/:celebrityId', async (req, res, next) => {
+  const { celebrityId } = req.params;
+  const { name, occupation, catchPhrase } = req.params;
+  try {
+    await Celebrity.findByIdAndUpdate(celebrityId, {name, occupation, catchPhrase}, {new:true});
+    res.redirect('/celebrities')
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET Delete celebrity */
+/* ROUTE /celebrities/delete/:celebrityId*/
+router.get('/delete/:celebrityId', async (req, res, next) => {
+  const { celebrityId } = req.params;
+  try {
+    await Celebrity.findById(celebrityId);
+    res.redirect('/celebrities');
+  } catch (error) {
+    next(error)
+  }
+});
 
 module.exports = router;
